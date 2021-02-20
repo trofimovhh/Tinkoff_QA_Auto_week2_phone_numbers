cdf4b4f162cc28bd7ffc1a737343829c4453da64
trofimovhh <trofimov.hh@gmail.com>
2021-02-20 23:16:05 +0300

    Some changes after review

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,26 +7,27 @@ public class Main {
         checkNumber(phoneNumber);
     }
 
-    public static void checkNumber(String phoneNumber){
+    public static void checkNumber(String phoneNumber) {
         String[] myResult = new String[2];
         String phoneChanges = "";
         phoneNumber = phoneNumber.strip();
         boolean isNotChanged = true;
-        if (phoneNumber.startsWith("+7")){
-            phoneNumber = phoneNumber.replaceFirst("\\+7","8");
+        if (phoneNumber.startsWith("+7")) {
+            phoneNumber = phoneNumber.replaceFirst("\\+7", "8");
             phoneChanges = phoneChanges.concat("Замена +7 на 8; ");
             isNotChanged = false;
         }
-        if (phoneNumber.contains(" ") || phoneNumber.contains("(") || phoneNumber.contains(")")){
+        if (phoneNumber.contains(" ") || phoneNumber.contains("(") || phoneNumber.contains(")")) {
             phoneNumber = phoneNumber.replaceAll("[\\s)(]", "");
             phoneChanges = phoneChanges.concat("В номере есть пробелы и/или скобки; ");
             isNotChanged = false;
         }
-        if (phoneNumber.length() != 11){
+        if (phoneNumber.length() != 11) {
             phoneNumber = "Введен некорректный номер";
             phoneChanges = phoneChanges.concat("Количество символов не равно 11");
             isNotChanged = false;
         }
+
         if (isNotChanged) {
             phoneChanges = "Не было изменений";
         }
